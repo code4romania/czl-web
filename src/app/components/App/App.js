e21eac902b649aa83e714f258364303920770b68
SergiuB <buciuc_sergiu@yahoo.com>
2018-04-21 23:06:23 +0300

    main content styling

--- src/app/components/App/App.js
+++ src/app/components/App/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { MuiThemeProvider } from 'material-ui/styles';
-import theme from '../../../theme';
+import Grid from 'material-ui/Grid';
+import { withStyles } from 'material-ui/styles';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 import AppBar from '../AppBar/AppBar';
@@ -10,8 +11,17 @@ import { CategoriesPage } from '../../../categories';
 import { ProposalsPage } from '../../../proposals';
 import { InstitutionsPage } from '../../../institutions';
 
+import theme from '../../../theme';
+
+const styles = theme => ({
+  content: {
+    padding: 10
+  }
+});
+
 class App extends Component {
   render() {
+    const { classes } = this.props;
     return (
       <MuiThemeProvider theme={theme}>
         <div />
@@ -19,10 +29,12 @@ class App extends Component {
           <div>
             <AppBar auth={{ user: { name: 'Alejandro' } }} />
 
-            <Route exact path="/" component={HomePage} />
-            <Route path="/categories" component={CategoriesPage} />
-            <Route path="/proposals" component={ProposalsPage} />
-            <Route path="/institutions" component={InstitutionsPage} />
+            <div className={classes.content}>
+              <Route exact path="/" component={HomePage} />
+              <Route path="/categories" component={CategoriesPage} />
+              <Route path="/proposals" component={ProposalsPage} />
+              <Route path="/institutions" component={InstitutionsPage} />
+            </div>
           </div>
         </Router>
       </MuiThemeProvider>
@@ -30,4 +42,4 @@ class App extends Component {
   }
 }
 
-export default App;
+export default withStyles(styles)(App);
