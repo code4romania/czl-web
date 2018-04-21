9b8befd6db49c1b5256998936369166565ae69ce
SergiuB <buciuc_sergiu@yahoo.com>
2018-04-21 14:06:09 +0300

    Material UI wrapper for stories

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,10 +1,34 @@
+import React from 'react';
 
-import { configure } from '@storybook/react';
+import { MuiThemeProvider, createMuiTheme } from 'material-ui/styles';
+import purple from 'material-ui/colors/purple';
+import green from 'material-ui/colors/green';
 
-const req = require.context('../src/stories', true, /\.stories\.js$/)
+import { configure, addDecorator } from '@storybook/react';
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      light: purple[300],
+      main: purple[500],
+      dark: purple[700],
+    },
+    secondary: {
+      light: green[300],
+      main: green[500],
+      dark: green[700],
+    },
+  },
+});
+
+addDecorator(story => (
+  <MuiThemeProvider theme={theme}>{story()}</MuiThemeProvider>
+));
+
+const req = require.context('../src', true, /\.stories\.js$/);
 
 function loadStories() {
-  req.keys().forEach((filename) => req(filename))
+  req.keys().forEach(filename => req(filename));
 }
 
 configure(loadStories, module);
