3b16f305069282bbdf12f38ce1358b8834fafac7
SergiuB <buciuc_sergiu@yahoo.com>
2018-04-21 14:59:28 +0300

    extract theme in separate module

---
+++ src/theme.js
@@ -0,0 +1,20 @@
+import { createMuiTheme } from 'material-ui/styles';
+import purple from 'material-ui/colors/purple';
+import green from 'material-ui/colors/green';
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      light: purple[300],
+      main: purple[500],
+      dark: purple[700],
+    },
+    secondary: {
+      light: green[300],
+      main: green[500],
+      dark: green[700],
+    },
+  },
+});
+
+export default theme;
