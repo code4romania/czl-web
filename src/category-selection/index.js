437db36e82acc1ff7761665b85786bc6aa055d30
Andra Mititelu <mititelu.andra@gmail.com>
2018-04-21 17:22:11 +0300

    added initial category selection page

--- src/category-selection/index.js
+++ src/category-selection/index.js
@@ -1,2 +1,3 @@
 export { default as CategoryBox } from './components/CategoryBox/CategoryBox';
 export { default as CategoryBoxesList } from './components/CategoryBoxesList/CategoryBoxesList';
+export { default as CategorySelectionPage } from './components/CategorySelectionPage/CategorySelectionPage';
